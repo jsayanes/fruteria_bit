735a35338eb97ebc1eca3f83125a5deb19739f62
pasbfwin2601 <pasbfwin2601@gmail.com>
2021-02-20 22:32:26 -0300

    subida3 de proyecto de fruteria bit

---
+++ app/src/main/java/com/bit/fruteriabit/FrutaViewHolder.java
@@ -0,0 +1,29 @@
+package com.bit.fruteriabit;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+//FrutaViewHolder class
+public class FrutaViewHolder extends RecyclerView.ViewHolder{
+
+    private final TextView frutaItemView;
+
+   private FrutaViewHolder(View itemView) {
+       super(itemView);
+       frutaItemView = itemView.findViewById(R.id.textViewNombre);
+   }
+
+   public void bind (String texto) {
+        frutaItemView.setText(texto);
+   }
+
+   static FrutaViewHolder create(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruta_item, parent, false);
+        return new FrutaViewHolder(view);
+   }
+}
